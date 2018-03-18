f0418ced605805473b17eacc4e66af70412466bf
Sergei Litovchenko <serglit10229@gmail.com>
2018-03-17 22:39:50 -0700

    Initial commit

---
+++ Assets/AnimationController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+    public GameObject Rifle;
+
+    public bool Fire = false;
+
+	// Update is called once per frame
+	void Update () {
+        if (!Rifle.GetComponent<Animation>().isPlaying)
+        {
+            Rifle.GetComponent<Animation>().CrossFade("Idle");
+        }
+        if (Fire == true)
+        {
+            Rifle.GetComponent<Animation>().Stop("Idle");
+            Rifle.GetComponent<Animation>().Play("Fire");
+            if (!Rifle.GetComponent<Animation>().IsPlaying("Fire"))
+            {
+                Rifle.GetComponent<Animation>().Stop("Fire");
+            }
+        }
+        //InvokeRepeating("FireAnimation", 3, 300);
+    }
+
+    public void FireAnimation()
+    {
+        while (Rifle.GetComponent<Animation>().IsPlaying("Fire"))
+            //Rifle.GetComponent<Animation>().Stop();
+            Rifle.GetComponent<Animation>().CrossFade("Fire");
+        if (!Rifle.GetComponent<Animation>().IsPlaying("Fire"))
+        {
+            Rifle.GetComponent<Animation>().Stop();
+        }
+    }
+
+
+}
---
+++ Assets/BulletHoles.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletHoles : MonoBehaviour {
+
+    public GameObject[] bulletTex;
+	// Update is called once per frame
+	void Update () {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, fwd * 10, Color.green);
+
+        if (Input.GetMouseButtonDown(0) && Physics.Raycast(transform.position, fwd, out hit, 10))
+        {
+            Vector3 point = new Vector3(0.01f, 0.01f, 0.01f);
+            Instantiate(bulletTex[0], hit.point + hit.normal*0.01f, Quaternion.FromToRotation(Vector3.up, hit.normal));
+
+            Debug.Log(hit.normal);
+        }
+	}
+}
